a33598c18cbf145a4fe2f1da1612c901ec93c126
CIANDT\janainaim <janainaim@ciandt.com>
2022-05-25 17:42:42 -0300

    Última revisão das tarefas essenciais e adição de algumas anotações sobre o que foi feito e ideias futuras

--- src/main/java/br/com/alura/comex/baseDados/ProcessadorAdapter.java
+++ src/main/java/br/com/alura/comex/baseDados/ProcessadorAdapter.java
@@ -2,7 +2,6 @@ package br.com.alura.comex.baseDados;
 
 import br.com.alura.comex.model.Pedido;
 
-import java.io.ObjectStreamException;
 import java.util.List;
 
 public abstract class ProcessadorAdapter {
--- src/main/java/br/com/alura/comex/service/ProcessadorService.java
+++ src/main/java/br/com/alura/comex/service/ProcessadorService.java
@@ -1,7 +1,6 @@
 package br.com.alura.comex.service;
 
 import br.com.alura.comex.baseDados.ProcessadorAdapter;
-import br.com.alura.comex.model.Pedido;
 
 import java.util.List;
 
