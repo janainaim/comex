5efcf42643ec28a361889fdf290dc2b2020e3c1b
CIANDT\janainaim <janainaim@ciandt.com>
2022-06-01 12:23:12 -0300

    Implementação do TestDataBuilder e pequena refatoração do RelatorioDeVendasPorCategoriaTest

--- src/main/test/br/com/alura/comex/service/relatorios/RelatorioDeVendasPorCategoriaTest.java
+++ src/main/test/br/com/alura/comex/service/relatorios/RelatorioDeVendasPorCategoriaTest.java
@@ -2,52 +2,62 @@ package br.com.alura.comex.service.relatorios;
 
 import br.com.alura.comex.baseDados.ProcessadorAdapter;
 import br.com.alura.comex.model.Pedido;
+import br.com.alura.comex.model.PedidoBuilder;
 import br.com.alura.comex.service.processador.ProcessadorDeCsv;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.*;
 
+import static org.junit.jupiter.api.Assertions.*;
 public class RelatorioDeVendasPorCategoriaTest {
+
+    private RelatorioDeVendasPorCategoria service;
+
+    public void iniciaRelatorioDeVendasPorCategoria(List<Pedido> pedidos){
+        this.service = new RelatorioDeVendasPorCategoria(pedidos, System.out::println);
+        service.gerarRelatorio();
+    }
+
 
     @Test
     void deveRetornarRelatorioComListaVaziaDePedidos(){
-        //1-Arrange
+        //1-Arrange em comum nos testes
         List<Pedido> pedidos = new ArrayList<>();
-        RelatorioDeVendasPorCategoria vendasPorCategoria = new RelatorioDeVendasPorCategoria(pedidos, System.out::println);
+        RelatorioDeVendasPorCategoria relatorioDeVendasPorCategoria =
+                new RelatorioDeVendasPorCategoria(pedidos, System.out::println);
 
         //3-Assert e 2-Act(vendasPorCategoria.gerarRelatorio()) que está como methodReference
-        Assertions.assertThrows(IllegalArgumentException.class,
-                vendasPorCategoria::gerarRelatorio);
+        assertThrows(IllegalArgumentException.class,
+                relatorioDeVendasPorCategoria::gerarRelatorio);
     }
 
     @Test
     void deveRetornarRelatorioComListaDeUmUnicoPedido(){
         //1-Arrange
-        Pedido pedido = new Pedido("LIVROS",
-                "KINDLE",
-                "ANA",
-                new BigDecimal("200"),
-                3,
-                LocalDate.now());
+        List<Pedido> pedidos = List.of(new PedidoBuilder()
+                .categoria("LIVROS")
+                .produto("KINDLE")
+                .cliente("ANA")
+                .preco(new BigDecimal("200"))
+                .quantidade(3)
+                .data(LocalDate.now())
+                .build());
 
-        List<Pedido> pedidos = List.of(pedido);
-        RelatorioDeVendasPorCategoria vendasPorCategoria = new RelatorioDeVendasPorCategoria(pedidos, System.out::println);
+        iniciaRelatorioDeVendasPorCategoria(pedidos);
 
         //2-Act
-        vendasPorCategoria.gerarRelatorio();
-        List<VendasPorCategoria> resultado = vendasPorCategoria.getVendasPorCategoria();
-        VendasPorCategoria vendasPorCategoria1 = resultado.get(0);
+        List<VendasPorCategoria> resultado = service.getVendasPorCategoria();
+        VendasPorCategoria vendasPorCategoriaRetorno = resultado.get(0);
 
         //3-Assert
         //Retorno esperado de apenas um item na lista
-        Assertions.assertEquals(1, resultado.size());
+        assertEquals(1, resultado.size());
+
         //Comparação dos itens do pedido
-        Assertions.assertEquals("LIVROS", vendasPorCategoria1.getCategoria());
-        Assertions.assertEquals(3, vendasPorCategoria1.getQuantidadeVendida());
-        //Assertions.assertEquals("600", vendasPorCategoria1.getValorMontante());
+        assertEquals("LIVROS", vendasPorCategoriaRetorno.getCategoria());
+        assertEquals(3, vendasPorCategoriaRetorno.getQuantidadeVendida());
+        assertEquals(new BigDecimal("600"), vendasPorCategoriaRetorno.getValorMontante());
 
     }
 
@@ -57,16 +67,54 @@ public class RelatorioDeVendasPorCategoriaTest {
         ProcessadorAdapter processadorAdapter = new ProcessadorDeCsv();
         List<Pedido> pedidos = processadorAdapter.listarPedidos("pedidos");
 
-        RelatorioDeVendasPorCategoria vendasPorCategoria = new RelatorioDeVendasPorCategoria(pedidos, System.out::println);
+        iniciaRelatorioDeVendasPorCategoria(pedidos);
 
         //2-Act
-        vendasPorCategoria.gerarRelatorio();
-        List<VendasPorCategoria> resultado = vendasPorCategoria.getVendasPorCategoria();
-        VendasPorCategoria vendasPorCategoria1 = resultado.get(0);
+        List<VendasPorCategoria> resultado = service.getVendasPorCategoria();
 
         //3-Assert
         //Teste se nenhum dado retorna nulo
-        Assertions.assertNotNull(resultado);
+        assertNotNull(resultado);
+
+    }
+
+    @Test
+    void deveRetornarDuasCategoriasComTresPedidosAoTodo(){
+        List<Pedido> pedidos = List.of(new PedidoBuilder()
+                .categoria("LIVROS")
+                .produto("KINDLE")
+                .cliente("ANA")
+                .preco(new BigDecimal("200"))
+                .quantidade(3)
+                .data(LocalDate.now())
+                .build(),
+                new PedidoBuilder()
+                        .categoria("INFORMÁTICA")
+                        .produto("MOUSE GAMER LOGTECH")
+                        .cliente("JAMILA")
+                        .preco(new BigDecimal("300"))
+                        .quantidade(1)
+                        .data(LocalDate.now())
+                        .build(),
+                new PedidoBuilder()
+                        .categoria("LIVROS")
+                        .produto("LIVROS DIVERSOS")
+                        .cliente("TAMILA")
+                        .preco(new BigDecimal("122.80"))
+                        .quantidade(5)
+                        .data(LocalDate.now())
+                        .build());
+
+        iniciaRelatorioDeVendasPorCategoria(pedidos);
+
+        //2-Act
+        List<VendasPorCategoria> resultado = service.getVendasPorCategoria();
+
+        //Verifica se os três pedidos retornaram um relatório com duas categorias
+        assertEquals(2, resultado.size());
+
+        //Verifica se o montante da categoria LIVROS está correta conforme a soma dos pedidos e os seus respetivos preços
+        assertEquals(new BigDecimal("1214.00"), resultado.get(1).getValorMontante());
 
     }
 
