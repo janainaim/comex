17a17b1748279fb48ec2eb254de6be43c7baff0c
CIANDT\janainaim <janainaim@ciandt.com>
2022-07-20 19:16:07 -0300

    Teste de comunicação de microsserviços síncronos funcional

--- src/main/java/br/com/alura/comex/infra/produto/ProdutoController.java
+++ src/main/java/br/com/alura/comex/infra/produto/ProdutoController.java
@@ -5,10 +5,15 @@ import br.com.alura.comex.dominio.produto.ProdutoForm;
 import br.com.alura.comex.dominio.produto.Produto;
 import br.com.alura.comex.dominio.categoria.CategoriaRepository;
 import br.com.alura.comex.dominio.produto.ProdutoRepository;
+
+import java.math.BigDecimal;
 import java.net.URI;
 import java.util.List;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
+
+import br.com.alura.comex.infra.pagamento.PagamentoDto;
+import br.com.alura.comex.infra.pagamento.PagamentoRestClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -28,6 +33,11 @@ public class ProdutoController {
   @Autowired
   CategoriaRepository categoriaRepository;
 
+  //Coloquei em produto porque não tinha feito a desejável de pedido,
+  // a ideia é apenas ver se funciona a integração (farei com kafka tbm se der tempo)
+  @Autowired
+  private PagamentoRestClient pagamentoRestClient;
+
   @GetMapping
   public List<ProdutoDto> listaDeProdutos() {
     Pageable pageable = PageRequest.of(0, 5, Sort.by(Sort.Direction.ASC, "nome"));
@@ -43,6 +53,12 @@ public class ProdutoController {
 
     Produto produto = form.converter(categoriaRepository);
     produtoRepository.save(produto);
+
+    pagamentoRestClient.cria(PagamentoDto.builder()
+            .pedidoId(2l)
+            .cpfCliente("111.111.111-22")
+            .valor(BigDecimal.valueOf(2000l))
+            .build());
 
     URI uri = uriBuilder.path("/produtos/{id}").buildAndExpand(produto.getId()).toUri();
     return ResponseEntity.created(uri).body(new ProdutoDto(produto));
